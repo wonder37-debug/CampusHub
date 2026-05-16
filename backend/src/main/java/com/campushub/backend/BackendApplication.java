1a579dbba2fd167e6c5b1cd991b9f6b0b8b6e604
wonderfulwings <yihangwang_nju@smail.nju.edu.cn>
2026-05-16 20:38:12 +0800

    chore(db): 初始化本地 MySQL 表结构,添加建表初始化SQL和使用说明并隔离本地敏感密码配置

--- backend/src/main/java/com/campushub/backend/BackendApplication.java
+++ backend/src/main/java/com/campushub/backend/BackendApplication.java
@@ -1,15 +1,13 @@
 package com.campushub.backend;
 
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class})
+
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
